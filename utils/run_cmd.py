fbd91ae3da45f58861c5ab410bd56d42768f5575
ksumarshmallow <ksumarshmallow2020@gmail.com>
2024-09-13 18:51:16 +0300

    fix run cmd

--- utils/run_cmd.py
+++ utils/run_cmd.py
@@ -1,5 +1,11 @@
 import subprocess
 
-def run_cmd(cmd):
-    result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+def run_cmd(cmd, input_data=None, capture_output=True, text=True, check=True, shell=False,**kwargs):
+    result = subprocess.run(cmd, 
+                            input=input_data, 
+                            capture_output=capture_output, 
+                            text=text, 
+                            check=check,
+                            shell=shell,
+                            **kwargs)
     return result
